4b7d977f7ddb373b011965b905830ea36feee0a7
yuu8787 <yuuma.1006saru@gmail.com>
2023-05-11 10:45:44 +0900

    db

--- site/php/common.php
+++ site/php/common.php
@@ -1,7 +1,7 @@
 <?php
     // データベースに接続する
     try{
-        $db=new PDO('mysql:dbname=instagram;host=127.0.0.1;charset=utf8','root','');
+        $db=new PDO('mysql:dbname=mealfriend;host=127.0.0.1;charset=utf8','root','');
     }catch(PDOException $e){
         echo 'DB接続エラー：'.$e->getMessage();
     }
