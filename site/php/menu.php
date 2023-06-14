14eb28dd380fcc0334fc45ccf901196541f46167
noncha-ts023 <2a3np.c08qt@gmail.com>
2023-06-14 18:12:46 +0900

    メニュー一覧レスポンシブ対応

--- site/php/menu.php
+++ site/php/menu.php
@@ -52,145 +52,143 @@ while ($cart = $carts->fetch()) {
 <body>
 
 
-        <header class="py-3 mb-3 border-bottom flex-top">
-            <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
-                <a href="#" class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 link-dark text-decoration-none" id="dropdownNavLink" data-bs-toggle="dropdown" aria-expanded="false">
-                    
-                    <!-- ロゴ画像を置く -->
-                    <img src="../images/logo.png" class="bi me-2" width="140" height="32">
-                </a>
+    <header class="py-3 mb-3 border-bottom flex-top">
+        <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
+            <a href="#" class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 link-dark text-decoration-none" id="dropdownNavLink" data-bs-toggle="dropdown" aria-expanded="false">
+
+                <!-- ロゴ画像を置く -->
+                <img src="../images/logo.png" class="bi me-2" width="140" height="32">
+            </a>
 
 
-                <div class="d-flex justify-content-end">
+            <div class="d-flex justify-content-end">
 
 
 
 
-                    <!-- ログインしていないときに表示 -->
-                    <?php
-                    if (!isset($_SESSION["id"])) {
-                    ?>
-                        <a class="text-end" href="../php/login.php">
-                            <button type="button" class="btn btn-outline-primary">Login/Sign-up</button>
-                        </a>
-                    <?php
+                <!-- ログインしていないときに表示 -->
+                <?php
+                if (!isset($_SESSION["id"])) {
+                ?>
+                    <a class="text-end" href="../php/login.php">
+                        <button type="button" class="btn btn-outline-primary">Login/Sign-up</button>
+                    </a>
+                <?php
 
 
-                    } else {
-                    ?>
+                } else {
+                ?>
                     <!-- ログインしているときに表示 -->
-                        <div class="flex-shrink-0 dropdown">
-                            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                                <!-- ログインした人の画像 -->
-                                <img src="../images/icon.jpg" alt="mdo" width="32" height="32" class="rounded-circle">
-                            </a>
-                            <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-                                <li><a href="my_page.php"><?php print ($user["f_user_name"]) . "のマイページ"; ?></a></li>
-                                <p id="userId" style="display: none;"><?php print($user["f_user_id"]); ?></p>
+                    <div class="flex-shrink-0 dropdown">
+                        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                            <!-- ログインした人の画像 -->
+                            <img src="../images/icon.jpg" alt="mdo" width="32" height="32" class="rounded-circle">
+                        </a>
+                        <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                            <li><a href="my_page.php"><?php print ($user["f_user_name"]) . "のマイページ"; ?></a></li>
+                            <p id="userId" style="display: none;"><?php print($user["f_user_id"]); ?></p>
 
-                                <li><a class="dropdown-item" href="#">Settings</a></li>
-                                <li><a class="dropdown-item" href="#">Profile</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Sign out</a></li>
-                            </ul>
-                        </div>
-
-                    <?php
-                    }
-                    ?>
-
-
-
-                    <div>
-                        <a href="cart.php"><img class="headerimg" src="../images/cart.jpg" alt="カート"></a>
+                            <li><a class="dropdown-item" href="#">Settings</a></li>
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Sign out</a></li>
+                        </ul>
                     </div>
-                </div>
-            </div>
-        </header>
 
-
-
-
-        <main>
-
-            <div id="box">
-
-                <h3>メニュー選択</h3>
-
-                <select name='genre'>
-                    <option value='all'>すべて</option>
-                    <?php
-                    for ($i = 0; $genre = $genres->fetch(); $i++) {
-                        print('<option value={$i}>' . $genre['f_item_genre_name'] . '</option>');
-                    }
-                    ?>
-                </select>
-
-                <select name='allergen'>
-                    <option value='filter'>食材フィルタ</option>
-                    <?php
-                    for ($i = 0; $i != count($allergens); $i++) {
-                        print('<option value={$i}>' . $allergens[$i] . '</option>');
-                    }
-                    ?>
-                </select>
-
-                <select name='allergen'>
-                    <option value='filter'>ソート</option>
-                    <?php
-                    for ($i = 0; $i != count($sorts); $i++) {
-                        print('<option value={$i}>' . $sorts[$i] . '</option>');
-                    }
-                    ?>
-                </select>
+                <?php
+                }
+                ?>
 
                 <div>
-                    <table>
+                    <a href="cart.php"><img class="headerimg" src="../images/cart.jpg" alt="カート"></a>
+                </div>
+            </div>
+        </div>
+    </header>
 
-                        <p id="userId" style="display: none;"><?php print($user["f_user_id"]); ?></p>
 
-                        <?php
-                        // print("<h1>");
-                        // print_r($cartItemId);
-                        // print("<h1>");
-                        for ($i = 0; $item = $items->fetch(); $i++) {
-                            if ($i % 4 == 0) {
-                                print('<tr>');
-                            }
-                            print('<td>');
-                            print('<a href="item_piece.php?id=' . $item['f_item_id'] . '" id="itemId' . $item['f_item_id'] . '">');
-                            print('<img id="menu_img" src="../images/items/' . $item['f_item_image'] . '.jpg" alt=' . $item['f_item_name'] . '>');
-                            print('<p>' . $item['f_item_name'] . '</p>');
-                            print('<p>' . $item['f_item_price'] . '円</p>');
-                            print('</a>');
-                            if (isset($user["f_user_id"])) {
-                                if (in_array($i + 1, $cartItemId)) {
-                                    print('<a href="./cart.php">カートに移動する</a>');
-                                } else {
-                                    print('<button onClick="inCart(this)">カートに入れる</button>');
-                                }
-                            }
-                            print('</td>');
-                            if ($i % 4 == 3) {
-                                print('</tr>');
+
+
+    <main>
+
+        <div id="box">
+
+            <h3>メニュー選択</h3>
+
+            <select name='genre'>
+                <option value='all'>すべて</option>
+                <?php
+                for ($i = 0; $genre = $genres->fetch(); $i++) {
+                    print('<option value={$i}>' . $genre['f_item_genre_name'] . '</option>');
+                }
+                ?>
+            </select>
+
+            <select name='allergen'>
+                <option value='filter'>食材フィルタ</option>
+                <?php
+                for ($i = 0; $i != count($allergens); $i++) {
+                    print('<option value={$i}>' . $allergens[$i] . '</option>');
+                }
+                ?>
+            </select>
+
+            <select name='allergen'>
+                <option value='filter'>ソート</option>
+                <?php
+                for ($i = 0; $i != count($sorts); $i++) {
+                    print('<option value={$i}>' . $sorts[$i] . '</option>');
+                }
+                ?>
+            </select>
+
+            <div>
+                <div class="container">
+
+                    <p id="userId" style="display: none;"><?php print($user["f_user_id"]); ?></p>
+                    
+                    <div class="row">
+                    <?php
+                    // print("<h1>");
+                    // print_r($cartItemId);
+                    // print("<h1>");
+                    for ($i = 0; $item = $items->fetch(); $i++) {
+
+                        // print('<div class="row">');
+
+                        print('<div class="col-sm-6 col-md-3 col-lg-2">');
+                        print('<a href="item_piece.php?id=' . $item['f_item_id'] . '" id="itemId' . $item['f_item_id'] . '">');
+                        print('<img id="menu_img" src="../images/items/' . $item['f_item_image'] . '.jpg" alt=' . $item['f_item_name'] . '>');
+                        print('<p>' . $item['f_item_name'] . '</p>');
+                        print('<p>' . $item['f_item_price'] . '円</p>');
+                        print('</a>');
+                        if (isset($user["f_user_id"])) {
+                            if (in_array($i + 1, $cartItemId)) {
+                                print('<a href="./cart.php">カートに移動する</a>');
+                            } else {
+                                print('<button onClick="inCart(this)">カートに入れる</button>');
                             }
                         }
-                        ?>
+                        print('</div>');
 
-                    </table>
+                        // print('</div>');
+                    }
+                    ?>
 
                 </div>
 
             </div>
 
-        </main>
+        </div>
 
-        <footer>
-            <!-- コピーライト -->
-            <small>&copy; 2023 ミールフレンド all right reserved</small>
-        </footer>
+    </main>
+
+    <footer>
+        <!-- コピーライト -->
+        <small>&copy; 2023 ミールフレンド all right reserved</small>
+    </footer>
 
 
 
