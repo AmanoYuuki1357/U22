7bd89595283e288ef0ec38ab8f7a7adbe04e5095
AmanoYuuki1357 <awan.1357@gmail.com>
2023-06-15 10:50:50 +0900

    headerできん

--- site/php/header.php
+++ site/php/header.php
@@ -1,17 +1,12 @@
-
-<header class="py-3 mb-3 border-bottom flex-top">
+    <header class="py-3 mb-3 border-bottom flex-top">
         <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
-            <a href="../php/menu.php" class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 link-dark text-decoration-none" id="dropdownNavLink" data-bs-toggle="dropdown" aria-expanded="false">
 
-                <!-- ロゴ画像を置く -->
+            <!-- ロゴ画像を置く -->
+            <a href="./menu.php" class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 link-dark text-decoration-none" id="dropdownNavLink" data-bs-toggle="dropdown" aria-expanded="false">
                 <img src="../images/logo.png" class="bi me-2" width="140" height="32">
             </a>
 
-
             <div class="d-flex justify-content-end">
-
-
-
 
                 <!-- ログインしていないときに表示 -->
                 <?php
