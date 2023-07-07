1fa2c61f00f27dacbfbf26804d1200f5f82eec4f
noncha-ts023 <2a3np.c08qt@gmail.com>
2023-07-07 12:07:58 +0900

    なおったwWWWWWW

--- site/php/buy_history.php
+++ site/php/buy_history.php
@@ -29,62 +29,123 @@ $item = $items->fetchAll();
     <title>配送状況</title>
     <link rel="stylesheet" type="text/css" href="../css/reset.css">
     <!-- bootstrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link rel="stylesheet" href="../css/common.css">
+    <link rel="stylesheet" href="../css/buy_history.css">
     <!-- <link rel="stylesheet" type="text/css" href="../css/delivery_situation.css"> -->
 
 </head>
 
 <body>
-<!-- ヘッダー部分 -->
-<?php
-require('header.php');
-?>
+    <!-- ヘッダー部分 -->
+    <?php
+    require('header.php');
+    ?>
 
     <main>
-        <!-- <p><?php print_r($item); ?></p> -->
+
         <h2>購入履歴</h2>
 
         <?php
-            $cccOld = "";
-            $cccNew = "";
-            for($i=0; $i<count($item); $i++){
-                $cccNew = $item[$i]['f_buy_history_date'];
-                if($cccOld != $item[$i]['f_buy_history_date']){
-                    print("<div class='situation'>");
-                    print("<p>購入日：".$item[$i]['f_buy_history_date']."</p>");
-                    print("<div class='info'>");
-                    print("<p>商品名</p>");
-                    print("<ul>");
-                    print("<li>".$item[$i]['f_item_name']."×".$item[$i]['f_buy_history_num']."</li>");
-                }
+        // $cccOld = "";
+        // $cccNew = "";
+        // for ($i = 0; $i < count($item); $i++) {
+        //     $cccNew = $item[$i]['f_buy_history_date'];
+        //     if ($cccOld != $item[$i]['f_buy_history_date']) {
+        //         print("<div class='situation'>");
+        //         print("<p>購入日：" . $item[$i]['f_buy_history_date'] . "</p>");
+        //         print("<div class='info'>");
+        //         print("<p>商品名</p>");
+        //         print("<ul>");
+        //         // 一つ目の要素が出てくる
+        //         print("<li>" . $item[$i]['f_item_name'] . "×" . $item[$i]['f_buy_history_num'] . "</li>");
+        //     }
 
-                for($j=$i; $j<count($item); $j++){
-                    if($j != $i){
-                        if($cccNew == $item[$j]['f_buy_history_date']){
-                            print("<li>".$item[$j]['f_item_name']."×".$item[$i]['f_buy_history_num']."</li>");
-                        }
+        //     // for($j=$i; $j<count($item); $j++){
+        //     //     if($j != $i){
+        //     //         if($cccNew == $item[$j]['f_buy_history_date']){
+        //     //             print("<li>".$item[$j]['f_item_name']."×".$item[$j]['f_buy_history_num']."</li>");
+        //     //         }
+        //     //     }
+        //     // }
+
+
+        //     // ２個目以降の表示
+        //     $count = 0;
+        //     $count-=1;
+
+        //     for ($j = $i; $j < count($item); $j++) {
+        //         if ($j != $i) {
+        //             if ($cccNew == $item[$j]['f_buy_history_date']) {
+        //                 $count++;
+        //                 print("<li>" . $item[$j]['f_item_name'] . "×" . $item[$j]['f_buy_history_num'] . "</li>");
+        //             } else {
+        //                 break;
+        //             }
+        //         }
+        //     }
+
+
+
+
+
+
+
+
+
+
+
+        $cccOld = "";
+        $cccNew = "";
+        for ($i = 0; $i < count($item); $i++) {
+            $cccNew = $item[$i]['f_buy_history_date'];
+            if ($cccOld != $cccNew) {
+                $cccOld = $cccNew;
+                print("<div class='situation'>");
+                print("<p>購入日：" . $item[$i]['f_buy_history_date'] . "</p>");
+                print("<div class='info'>");
+                print("<p>商品名</p>");
+                print("<ul>");
+                // 一つ目の要素が出てくる
+                print("<li>" . $item[$i]['f_item_name'] . "×" . $item[$i]['f_buy_history_num'] . "</li>");
+
+
+                $count = 1;
+                for ($j = $i + 1; $j < count($item); $j++) {
+                    if ($cccNew == $item[$j]['f_buy_history_date']) {
+                        print("<li>" . $item[$j]['f_item_name'] . "×" . $item[$j]['f_buy_history_num'] . "</li>");
+                        $count++;
+                    } else {
+                        break;
                     }
                 }
 
-                if($cccOld != $item[$i]['f_buy_history_date']){
                 print("</ul>");
-                print("<p>配送予定：".$item[$i]['f_buy_history_delivery_situation']."</p>");
-                print("<p>配達場所：".$item[$i]['f_buy_history_delivery_place']."</p>");
                 print("</div>");
+
+
+
+                
+            
+
+
+
+                print("</ul>");
+                print("<p>配送予定：" . $item[$i]['f_buy_history_delivery_situation'] . "</p>");
+                print("<p>配達場所：" . $item[$i]['f_buy_history_delivery_place'] . "</p>");
+
                 print("</div>");
-                }
-                $cccOld = $item[$i]['f_buy_history_date'];
             }
+            $cccOld = $item[$i]['f_buy_history_date'];
+        }
         ?>
 
     </main>
 
-        <footer>Copyright 2023 mealfriend. All Rights Reserved.</footer>
+    <footer>Copyright 2023 mealfriend. All Rights Reserved.</footer>
 
-            <!-- コンテンツが短い時にfooterをwindow最下部に固定する -->
+    <!-- コンテンツが短い時にfooterをwindow最下部に固定する -->
     <script>
         // mainタグの高さを取得する
         var mainHeight = document.querySelector('main').clientHeight;
@@ -98,9 +159,7 @@ require('header.php');
 
 
     <!-- bootstrap CDN -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
