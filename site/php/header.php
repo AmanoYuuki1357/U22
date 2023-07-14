ebe00db2ebef2032b37579f25dcf15082c0fd444
noncha-ts023 <2a3np.c08qt@gmail.com>
2023-07-14 12:01:23 +0900

    ヘッダーいい感じ

--- site/php/header.php
+++ site/php/header.php
@@ -1,59 +1,63 @@
-<header class="py-3 mb-3 border-bottom flex-top">
-        <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
+<header>
 
-            <!-- ロゴ画像を置く -->
-            <a href="../php/menu.php" class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 text-decoration-none">
-                <img src="../images/logo.png" class="bi me-2" width="140" height="32">
-            </a>
 
-            <div class="d-flex justify-content-end">
+    <div class="container">
+        <div class="row header">
 
-                <!-- ログインしていないときに表示 -->
-                <?php
-                if (!isset($_SESSION["id"])) {
-                ?>
-                    <a class="text-end" href="../php/login.php">
-                        <button type="button" class="btn btn-outline-primary">ログイン/サインアップ</button>
+            <?php
+            if (!isset($_SESSION["id"])) {
+            ?>
+                <a class="text-end" href="../php/login.php">
+                    <button type="button" class="btn btn-outline-primary">ログイン/サインアップ</button>
+                </a>
+            <?php
+
+
+            } else {
+            ?>
+
+
+                <!-- rogo画像 -->
+                <div class="col-6">
+                    <a class="logoImg" href="../php/menu.php" class="col">
+                        <img class="logoImg" src="../images/logo.png">
                     </a>
-                <?php
+                </div>
+
+                <!-- マイページ -->
+                <a class="col-2 hover-underline" href="my_page.php"><?php print ($user['f_user_nick_name']) . "様のマイページ"; ?></a>
 
 
-                } else {
-                ?>
-                    <!-- ログインしているときに表示 -->
-                    <div class="flex-shrink-0 dropdown icon">
-                        <a class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-                            <!-- ログインした人の画像 -->
-                            <img src="../images/icon.jpg" alt="mdo" width="32" height="32" class="rounded-circle">
-                        </a>
-                        <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-                            <li><a href="my_page.php"><?php print ($user['f_user_nick_name']) . "様のマイページ"; ?></a></li>
-                            <p id="userId" style="display: none;"><?php print($user["f_user_id"]); ?></p>
 
-                            <!-- 一時的にここに置く -->
-                            <!-- <li><a class="dropdown-item" href="./index.php">インデックス</a></li> -->
-                            <li><a class="dropdown-item" href="./menu.php">メニュー一覧</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="logout.php">サインアウト</a></li>
-                        </ul>
-                    </div>
 
-                <?php
-                }
-                ?>
+                <!-- メニュー一覧 -->
+                <a class="col-2 hover-underline" href="./menu.php">メニュー一覧</a>
 
-                <!-- ログインしているときに表示 -->
+
+            <?php
+            }
+            ?>
+
+            <div class="col-1"></div>
+
+            <div class="col-1">
                 <?php
                 if (isset($_SESSION["id"])) {
                 ?>
-                    <div>
-                        <a href="cart.php"><img class="headerimg" src="../images/cart.jpg" alt="カート"></a>
-                    </div>
-                <?php
-                }
-                ?>
+                    
+                        <a class="text-end" href="cart.php"><img class="headerimg" src="../images/cart.jpg" alt="カート"></a>
+                
+                <?php } ?>
+
             </div>
+
+
+
         </div>
-    </header>
+    </div>
+
+
+
+    <!-- サインアウトはマイページへ移動 -->
+
+</header>
